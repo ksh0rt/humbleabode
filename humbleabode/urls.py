5867bb6da36b6fd301123e2ed2ad25b0985474a5
ksh0rt <kshort2584@gmail.com>
2021-01-31 18:16:30 -0800

    Added user signup template/view

--- humbleabode/urls.py
+++ humbleabode/urls.py
@@ -13,14 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from adobes import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
