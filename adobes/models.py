0e74ba387e36c364b5c000e4baf3266781212943
ksh0rt <kshort2584@gmail.com>
2021-02-01 16:39:35 -0800

    Fixed abode model spelling and fix image upload issue

--- adobes/models.py
+++ adobes/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Abode(models.Model):
     price = models.IntegerField()
     street_address = models.CharField(max_length=255)
     city = models.CharField(max_length=20) #Longest city name in the US is 17 characters
-    stat = models.CharField(max_length=2) #Two for state abbreviations
+    state = models.CharField(max_length=2) #Two for state abbreviations
     zip_code = models.CharField(max_length=5) #Zipcodes are 5 digits long
     bedrooms = models.IntegerField()
     bathrooms = models.IntegerField()
     SqFoot = models.IntegerField()
-    image = models.ImageField(upload_to='humbleabode/images/')
+    image = models.ImageField(upload_to='humbleabode/images/', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- adobes/views.py
+++ adobes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
@@ -14,6 +14,11 @@ class SignUp(generic.CreateView):
 
 class AddAbode(generic.CreateView):
     model = Abode
-    fields = ['price','street_address','city','stat','zip_code','bedrooms','bathrooms','SqFoot','image']
+    fields = ['price','street_address','city','state','zip_code','bedrooms','bathrooms','SqFoot','image']
     template_name = 'abodes/add_abode.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(AddAbode, self).form_valid(form)
+        return redirect('home')
